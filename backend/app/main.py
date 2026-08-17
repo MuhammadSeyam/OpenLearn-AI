790f481777562753c32affea4dd819ee8d4a4f3e
0Abanoub <abanoubmakram47@gmail.com>
2026-08-17 11:15:49 +0300

    feat: add backend configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.config import settings
+
+app = FastAPI(
+    title=settings.app_name,
+)
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/health")
 async def health_check():
